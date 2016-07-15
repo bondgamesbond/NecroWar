14b950fd22dc33d41a46673f5105ccd9eddef110
Anıl Sert <anil.sert9@gmail.com>
2016-07-15 12:34:40 +0300

    Seperate Player Inputs in a different script

--- Scripts/Unit.cs
+++ Scripts/Unit.cs
@@ -1,19 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Unit : MonoBehaviour {
+public class Unit : MonoBehaviour
+{
 	public float HealthPoint;
 	public float AttackPoint;
 	public float AttackSpeed;
 	public GameObject Target;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
